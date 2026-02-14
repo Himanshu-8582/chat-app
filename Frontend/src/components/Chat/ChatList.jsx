5309091274ed7cb00e314956235d83ddff9135cd
Himanshu <himanshugupta8582@gmail.com>
2026-02-15 00:07:34 +0530

    SideBar Completed

--- Frontend/src/components/Chat/ChatList.jsx
+++ Frontend/src/components/Chat/ChatList.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useChatStore } from '../../store/useChatStore.js';
+import UserLodingSkeleton from '../../components/Chat/UserLoadingSkeleton';
+import NoChatsFound from '../../components/Chat/NoChatsFound';
 
 function ChatList() {
+  const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } = useChatStore();
+  useEffect(() => {
+    getMyChatPartners();
+  }, [getMyChatPartners]);
+  if (isUsersLoading) {
+    return <UserLodingSkeleton/>
+  }
+  if (chats.length === 0) return <NoChatsFound />
+  // console.log(chats)
   return (
-    <div>
-      
-    </div>
+    <>
+      {chats.map((chat) => (
+        <div key={chat._id}
+          className='bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors'
+          onClick={()=>setSelectedUser(chat)}
+        >
+          <div className='flex items-center gap-3'>
+            {/* TODO: Socket.IO */}
+            <div className={`avatar online`}>
+              <div className='size-12 rounded-full'>
+                <img src={chat.profilePic || '/avatar.png'} alt={chat.fullName} />
+              </div>
+            </div>
+            <h4 className='text-slate-200 font-medium truncate'>{chat.fullName}</h4>
+          </div>
+
+        </div>
+      ))}
+    </>
   )
 }
 
--- Frontend/src/components/Chat/NoConversationPlaceHolder.jsx
+++ Frontend/src/components/Chat/NoConversationPlaceHolder.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NoConversationPlaceHolder() {
   return (
     <div>
-      
+      NO Chat
     </div>
   )
 }
